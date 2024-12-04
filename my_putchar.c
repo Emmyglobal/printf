a38c6dc80c9753f85cfe7683bd04178ae3353329
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2024-12-04 03:18:49 -0800

    fixing

--- my_putchar.c
+++ my_putchar.c
@@ -3,11 +3,9 @@
 /**
  * my_putchar -> prints a character
  * @n: character to be printed
- * 
  * Return: integer
  */
-
 int my_putchar(char n)
 {
-    return (write(1, &n, 1));
+	return (write(1, &n, 1));
 }
